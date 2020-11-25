563928bc01f3bb4b338b1c2413628a7221605f16
Nguyen Dinh Tuan <ndinhtuan15@gmail.com>
2020-11-25 10:03:00 +0700

    Add Vietnamese comment in code

--- random_oracle_ensemble.py
+++ random_oracle_ensemble.py
@@ -7,8 +7,9 @@ from sklearn.svm import SVC
 import pickle
 from imblearn.over_sampling import SMOTE
 
+# Cài đặt thuật toán Random Oracle 
 class RandomOracleModel(object):
-
+    
     def __init__(self, base_learning='svm'):
         
         self.model1 = None
@@ -28,7 +29,8 @@ class RandomOracleModel(object):
         a = np.array(x1)
         b = np.array(x2)
         return np.linalg.norm(a-b)
-
+    
+    # Lưu 2 mô hình và 2 điểm tâm 
     def save(self, path_state_dict):
         
         model_state = {} 
@@ -40,7 +42,7 @@ class RandomOracleModel(object):
         with open(path_state_dict, 'wb') as file:
             pickle.dump(model_state, file)
             file.close()
-
+    # Load 2 mô hình và 2 điểm tâm
     def load(self, path_state_dict):
         
         with open(path_state_dict, 'rb') as file:
@@ -52,6 +54,7 @@ class RandomOracleModel(object):
 
     def train(self, X, Y):
         
+        # Chọn 2 điểm làm 2 tâm để chia dữ liệu huấn luyện 2 mô hình 
         len_train = len(X)
         i1 = np.random.randint(len_train)
         i2 = np.random.randint(len_train)
@@ -66,7 +69,8 @@ class RandomOracleModel(object):
         Y1 = []
         X2 = [] 
         Y2 = []
-
+        
+        # Chia dữ liệu ra 2 phần dựa vào khoảng cách với 2 điểm tâm 
         for x, y in zip(X, Y):
             if self.__distance(x, inst1) < self.__distance(x, inst2):
                 X1.append(x)
@@ -80,16 +84,19 @@ class RandomOracleModel(object):
         X2 = np.array(X2)
         Y2 = np.array(Y2)
         
+        # Huấn luyện 2 mô hình 
         print("Training Model 1")
         self.model1.fit(X1, Y1)
         print("Training Model 2")
         self.model2.fit(X2, Y2)
-
+    
+    # Dự đoán dựa vào 2 mô hình đã huấn luyện và 2 điểm tâm của 2 mô hình 
     def predict(self, x):
 
         assert self.model1 is not None and self.model2 is not None
         assert self.inst1 is not None and self.inst2 is not None
-
+        
+        # Chọn mô hình dự đoán dựa và khoảng cách giữa 2 điểm tâm 
         if self.__distance(x, self.inst1) < self.__distance(x, self.inst2):
             return self.model1.predict([x])[0]
         else:
@@ -108,23 +115,26 @@ class RandomOracleModel(object):
         preds = np.array(preds)
         
         return classification_report(y_test, preds)
-
+# Cai đặt thuật toán Adaboost kết hợp với adaboost
 class SmoteAdaboost(object):
 
     def __init__(self):
 
         self.adaboost_model = AdaBoostClassifier(n_estimator=10, random_state=42)
-
+    
+    # Huấn luyện mô hình Smote Adaboost 
     def fit(self, X, Y):
 
         smote = SMOTE(random_state=42)
-        X_sm, Y_sm = smote.fit_sample(X, Y)
+        X_sm, Y_sm = smote.fit_sample(X, Y) # Dùng smote để xử lý dữ liệu không cân bằng
         self.adaboost_model.fit(X_sm, Y_sm)
-
+    
+    # Hàm dự đoán cho Smote Adaboost 
     def predict(self, x):
         return self.adaboost_model.predict(x)
 
 # Show dataset and split dataset for training and testing
+# Phân tích và chi dữ liệu để huấn luyện và đánh giá mô hình.
 def data_analysis():
 
     df = pd.read_csv("emails.csv")
@@ -166,17 +176,20 @@ def test_smoteadaboost():
     train_x, test_x, train_y, test_y = data_analysis()
 
     # Try with single classifier
+    # Sử dụng mô hình chưa kết hợp cùng Random Oracle 
     smoteada = AdaBoostClassifier()
     smoteada.fit(train_x, train_y)
     y_pred = smoteada.predict(test_x)
     print("Accuracy Score for Single SVC: \n", classification_report(test_y, y_pred))
 
     # Try with Oracle Ensemble
+    # Sử dụng mô hình kết hợp với Random Oracle 
     random_oracle_model = RandomOracleModel(base_learning="smoteboost")
     random_oracle_model.train(train_x, train_y)
     print("Accuracy Score for Oracle ensemble: \n", random_oracle_model.evaluate(test_x, test_y))
 
     # Try Save and Load module
+    # Sử dụng chức năng saving và loading 
     random_oracle_model.save("model.pkl")
     rom = RandomOracleModel(base_learning="smoteboost")
     rom.load("model.pkl")
